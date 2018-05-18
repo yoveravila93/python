b6ea72bf0d6c2793c80ecdcce98cbc3cdef7e57b
yoveravila93 <38230484+yoveravila93@users.noreply.github.com>
2018-05-18 09:14:12 -0500

    python

---
+++ connction_s3_and_local_file.py
@@ -0,0 +1,70 @@
+import boto3
+import configparser
+from os import listdir
+from os.path import isfile, join
+
+config = configparser.ConfigParser()
+config.read('D:\\Documents\\GitHub\\Python\\config.ini')
+aws_key = config['DEFAULT']['aws_access_key_id']
+aws_secret_key = config['DEFAULT']['aws_secret_access_key']
+region = config['DEFAULT']['region']
+local_file = config['DEFAULT']['local_file']
+BUCKET = config['DEFAULT']['bucket']
+
+
+buck = []
+ruta = []
+delet = []
+file = ""
+comparacion = []
+
+#coneection by client
+session = boto3.Session(profile_name='default')
+s3Client = session.client('s3',region_name=region)
+s3Resource = session.resource('s3', region_name=region)   
+
+#connect bucket s3
+Bucket =s3Resource.Bucket(BUCKET)
+for obj in Bucket.objects.all():
+    buck.append(obj.key)
+    print (buck)
+    
+#connect local folder
+ruta = ([arch for arch in listdir(local_file) if isfile(join(local_file, arch))])
+
+#save file local in a list
+for  item in ruta:
+    if not item in buck:
+        comparacion.append(item)
+
+#load folders
+for file in comparacion:
+    rutas = local_file + "\\" + file
+    def upload_file():
+        s3Resource.Bucket(BUCKET).upload_file(rutas, file)
+        print ("creating files")
+
+    upload_file()
+    
+#delete files
+#see deleted files in the local folder
+
+for  item in buck:
+    if not item in ruta:
+        delet.append(item)
+
+#delete files
+for file in delet:
+    def delete_object():
+        s3Client.delete_object(Bucket=BUCKET, Key=file)
+        print ("deleting files")
+    delete_object()
+
+#update files
+for file in ruta:
+    rutas = local_file + "\\" + file
+    def update_file():
+        s3Resource.Bucket(BUCKET).upload_file(rutas, file)
+        print ("update files")
+
+    update_file()
---
+++ connection to RDS.py
@@ -0,0 +1,19 @@
+import pandas
+from sqlalchemy import create_engine
+import psycopg2 as pg
+
+#rds nueva
+#db_origin
+engine = create_engine("postgresql://root:Madara19@db-devso.ckfoflpkrdae.us-east-2.rds.amazonaws.com:5432/db_postdevso", echo = True, use_batch_mode = True, isolation_level="READ UNCOMMITTED", server_side_cursors=True)
+connect_db_postdevso = engine.connect()
+connect_db_postdevso = connect_db_postdevso.execution_options(isolation_level="READ COMMITTED")
+
+#new db
+engine = create_engine("postgresql://root:Madara19@db-connet.ckfoflpkrdae.us-east-2.rds.amazonaws.com:5432/db_connet", echo = True, use_batch_mode = True, isolation_level="READ UNCOMMITTED", server_side_cursors=True)
+connect_db_connect = engine.connect()
+connect_db_connect = connect_db_connect.execution_options(isolation_level="READ COMMITTED")
+
+chunksize = 1000
+data = pandas.read_sql("""select * from "tbl_indicator" order by 1""", connect_db_postdevso)
+
+data.to_sql('tbl_country', connect_db_connect, schema='public', if_exists='append', index=False, chunksize=chunksize)
